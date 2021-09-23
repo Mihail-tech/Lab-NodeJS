ee687f7929c5fffeed44c836b0a7fbd9624da4e9
ICX\Mikhail.Shpakouski <mihailshpakovskii@gmail.com>
2021-09-23 18:25:37 +0300

    fixed bags

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 /* GET home page. */
 router.get("/", (req, res) => {
   if (req.query.name) {
-    res.send(`<h1> hello ${ req.query.name }</h1>`);
+    res.send(`<p> hello ${ req.query.name }<p>`);
   } else {
     res.status(400).json({ name: "null", message: "You should write name" });
   }
