2eeb86ad84080ad0171c3c744235b6ef95a8b3d7
ICX\Mikhail.Shpakouski <mihailshpakovskii@gmail.com>
2021-09-28 15:54:33 +0300

    added routing and CRUD

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Lab-NodeJS
+1)
 Heroku
 https://serene-plains-21878.herokuapp.com/api/greetings?name=Mike
 
 Postman
 https://universal-shadow-734975.postman.co/workspace/My-Workspace~35f180b8-43db-440a-8b5d-dc3fd6254a00/request/17599613-e87da4d2-9ac4-4fef-96e2-5a2576dd7bf5
+
+2)
+Postman
+https://universal-shadow-734975.postman.co/workspace/My-Workspace~35f180b8-43db-440a-8b5d-dc3fd6254a00/collection/17599613-b961d042-8bc6-418f-ae4c-ec4001ee2a4d?ctx=documentation
+
--- routes/notes.js
+++ routes/notes.js
@@ -1,20 +1,63 @@
-import express from 'express';
+import express from "express";
+import Note from "../models/notes";
 
-const router = express.Router()
-
+const router = express.Router();
 
 //get
-router.get("/", (req, res) => { });
-  
-  //post
-router.post("/", (req, res) => { });
+//read note
+router.get("/", async (req, res) => {
+ try {
+   //return empty array
+  await res.status(200).send([]);
+  //return all notes
+  // const note = await Note.find()
+  // return res.json(note)
+ } catch (e) {
+  res.status(500).json(e)
+ }
+});
 
-  //put
-router.put("/:id", (req, res) => { });
+//post
+//create note
+router.post("/", async (req, res) => {
+  try {
+    const { title, content, timestamps } = req.body;
+    const note = await Note.create({ title, content, timestamps });
+    return res.status(201).json(note);
+  } catch (e) {
+    res.status(500).json(e)
+  }
+});
 
-  //delete
-router.delete("/:id", (req, res) => { });
+//put
+//update note
+//new: true, return version note by id
+router.put("/:id", async (req, res) => {
+ try {
+   const note = req.body
+  if (!note._id) {
+    res.status(400).json({message: 'нет ID'});
+  }
+  const update = await Note.findByIdAndUpdate(note._id, note, {new: true})
+  return res.status(200).json(update)
+ } catch (e) {
+  res.status(500).json(e)
+ }
+});
 
+//delete
+//delete chose id
+router.delete("/:id", async (req, res) => {
+  try {
+    const {id} = req.params
+    if (!id) {
+      res.status(400).json({message: 'нет ID'});
+    }
+    await Note.findByIdAndDelete(id)
+    return res.status(204).json( {success: true, id:id});
+  } catch (e) {
+    res.status(500).json(e)
+  }
+});
 
-
- export default router;
+export default router;
